4f160f276cf15b270ba1f31cd900c8a8cd3b33f3
d.sha <d.sha23@gmail.com>
2025-10-02 20:44:11 -0400

    Update Logo component to use new background-removed SVG file

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ export const Logo: React.FC<LogoProps> = ({
 }) => {
   return (
     <img 
-      src="/ORBIT_ADS_Logo__2_-removebg-preview.svg" 
+      src="/ChatGPT_Image_Oct_2__2025__08_34_04_PM-removebg-preview.svg" 
       alt={alt} 
       className={className}
     />
