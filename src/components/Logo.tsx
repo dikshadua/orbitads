3ae477730d0e7b91f94e7e90f3e4f22df9e187e9
d.sha <d.sha23@gmail.com>
2025-10-02 20:41:48 -0400

    Revert Logo component to previous SVG file

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -11,7 +11,7 @@ export const Logo: React.FC<LogoProps> = ({
 }) => {
   return (
     <img 
-      src="/ChatGPT Image Oct 2, 2025, 08_34_04 PM.svg" 
+      src="/ORBIT_ADS_Logo__2_-removebg-preview.svg" 
       alt={alt} 
       className={className}
     />
